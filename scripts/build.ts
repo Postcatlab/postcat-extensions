ad0a295d0327d0ed7bb1004f25811c26609e4361
bqy_fe <1743369777@qq.com>
2022-05-05 21:51:15 +0800

    chore: release v0.0.4

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,14 +1,19 @@
 import fs from 'fs'
 import { execa } from 'execa'
 
-const pkgs = fs.readdirSync('packages').filter((p) => {
+const argvs = process.argv.slice(2)
+console.log('process.argv', argvs)
+
+// 如果通过命令行传递了参数，则认为是指定打包，否则默认打包packages下所有包
+const pkgs = (argvs.length ? argvs : fs.readdirSync('packages')).filter((p) => {
   return fs.statSync(`packages/${p}`).isDirectory()
 })
 
-console.log(pkgs)
+console.log('pkgs', pkgs)
 
 const runParallel = (targets, buildFn) => {
   const res = []
+
   for (const target of targets) {
     res.push(buildFn(target))
   }
