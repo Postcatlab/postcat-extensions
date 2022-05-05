b775e5ec1f827651cfcd4ef3bd21e0eba5266f1a
bqy_fe <1743369777@qq.com>
2022-05-06 00:22:11 +0800

    refactor: project

---
+++ rollup.config.ts
@@ -0,0 +1,54 @@
+import path from 'path'
+import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+
+const pkg = process.env.TARGET
+
+const defaultBuildOptions = {
+  name: pkg || 'index',
+  formats: ['esm', 'cjs']
+}
+
+const resolve = (p) => {
+  return path.resolve(`${__dirname}/packages/${pkg}`, p)
+}
+const { buildOptions = defaultBuildOptions } = require(resolve('package.json'))
+
+const formatMap = {
+  esm: {
+    file: resolve(`dist/index.esm.js`),
+    format: 'esm'
+  },
+  cjs: {
+    file: resolve(`dist/index.cjs.js`),
+    format: 'cjs'
+  },
+  umd: {
+    file: resolve(`dist/index.js`),
+    format: 'umd'
+  }
+}
+const createConfig = (output) => {
+  output.name = buildOptions.name
+  return {
+    input: resolve('src/index.ts'),
+    output,
+    plugins: [
+      typescript(),
+      json(),
+      nodeResolve(),
+      // 配合 commnjs 解析第三方模块
+      nodeResolve(),
+      // 使得 rollup 支持 commonjs 规范，识别 commonjs 规范的依赖
+      commonjs()
+    ]
+  }
+}
+
+const configs = buildOptions.formats.map((format) =>
+  createConfig(formatMap[format])
+)
+
+export default configs
---
+++ scripts/publish.ts
@@ -0,0 +1,19 @@
+import { execSync } from 'child_process'
+import path from 'path'
+import consola from 'consola'
+import { version } from '../package.json'
+import { pkgs } from './utils'
+
+execSync('npm run build', { stdio: 'inherit' })
+
+let command = 'npm publish --access public'
+
+if (version.includes('beta')) command += ' --tag beta'
+
+for (const name of pkgs) {
+  execSync(command, {
+    stdio: 'inherit',
+    cwd: path.join('packages', name, 'dist')
+  })
+  consola.success(`Published @bqy/${name}`)
+}
