713630d81100d3ac7f7be141d5ba662977679582
yZaio <842499102@qq.com>
2023-04-06 20:11:11 +0800

    style: delete console

--- packages/postcat-import-eolink/src/index.ts
+++ packages/postcat-import-eolink/src/index.ts
@@ -2,7 +2,6 @@ import { EolinkImporter } from './EolinkImporter'
 
 export const importFunc = (data) => {
   const postmanImporter = new EolinkImporter(data)
-  console.log('postmanImporter', postmanImporter)
 
   return [postmanImporter.postcatData, null]
 }
