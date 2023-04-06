62946ea30cf45016bd2cb0b16ca8eba7299e8853
yZaio <842499102@qq.com>
2023-04-06 20:05:40 +0800

    fix: resolve import eolink bodyraw bug

--- packages/postcat-import-eolink/src/index.ts
+++ packages/postcat-import-eolink/src/index.ts
@@ -2,6 +2,7 @@ import { EolinkImporter } from './EolinkImporter'
 
 export const importFunc = (data) => {
   const postmanImporter = new EolinkImporter(data)
- 
+  console.log('postmanImporter', postmanImporter)
+
   return [postmanImporter.postcatData, null]
 }
