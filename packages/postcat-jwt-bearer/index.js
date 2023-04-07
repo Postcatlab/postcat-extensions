b2720e389c559d742758bf4d39ba514df103fdd9
yZaio <842499102@qq.com>
2023-04-07 18:06:36 +0800

    feat: jwt extension develop

--- packages/postcat-jwt-bearer/index.js
+++ packages/postcat-jwt-bearer/index.js
@@ -1,12 +1,20 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = {
   authAPI: (config) => {
-    console.log(config, 222);
 
-    // const headerValue = `Basic ${Buffer.from(config.username || '')}:${
-    //   config.password
-    // }`
-    return `pc.request.query.add({
-      key:'Authorization', value:'123'
-    })`
+    try {
+      const authorizationValue = `Bearer ${jwt.sign(
+        JSON.parse(config.payload),
+        config.Secret,
+        {algorithm: config.Algorithm}
+      )}`
+
+      return `pc.request.headers.add({
+        key:'Authorization', value:'${authorizationValue}'
+      })`
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
