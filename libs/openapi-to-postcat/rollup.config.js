f00d82c76180ece28eff86850e00b46ef843bce6
scarqin <1054139596@qq.com>
2023-04-07 18:47:57 +0800

    feat: package openapi to package

---
+++ libs/openapi-to-postcat/rollup.config.js
@@ -0,0 +1,26 @@
+import { terser } from 'rollup-plugin-terser'
+import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { name } from './package.json'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      name: name,
+      format: 'umd',
+      file: 'dist/index.js',
+      sourcemap: 'inline'
+    },
+    plugins: [nodeResolve(), esbuild({ target: 'esnext' }), terser()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'es',
+      file: 'dist/index.d.ts'
+    },
+    plugins: [dts()]
+  }
+]
