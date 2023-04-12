6e5bf964334b8189aca1c054839c82e59d760d67
yZaio <842499102@qq.com>
2023-04-12 16:08:50 +0800

    feat: jwt develop

--- packages/postcat-jwt-bearer/index.js
+++ packages/postcat-jwt-bearer/index.js
@@ -1,17 +1,31 @@
 const jwt = require('jsonwebtoken');
+const { createPrivateKey } = require('crypto');
 
 module.exports = {
   authAPI: (config) => {
     try {
-      const authorizationValue = `Bearer ${jwt.sign(
-        JSON.parse(config.payload),
-        config.isBase64Encoded ? Buffer.from(config.Secret).toString('base64') : config.Secret,
-        {algorithm: config.Algorithm}
-      )}`
+      const HsArr = ['HS256', 'HS384', 'HS512']
 
-      return `pc.request.headers.add({
-        key:'Authorization', value:'${authorizationValue}'
-      })`
+      const key = HsArr.includes(config.algorithm) ?
+        (config.isBase64Encoded ? Buffer.from(config.Secret).toString('base64') : config.Secret) :
+        createPrivateKey(config.Secret)
+
+
+      const authorizationValue = jwt.sign(
+        config.payload ? JSON.parse(config.payload) : {},
+        key,
+        {algorithm: config.algorithm, header: config.jwtHeaders ? JSON.parse(config.jwtHeaders) : {}}
+      )
+
+      if(config.tokenPosition === 'header') {
+        return `pc.request.headers.add({
+          key:'Authorization', value:'${config.headerPrefix || 'Bear'} ${authorizationValue}'
+        })`
+      } else {
+        return `pc.request.addQueryParams({
+          key: '${config.queryTokenName || 'token'}', value: '${authorizationValue}'
+        })`
+      }
     } catch (err) {
       console.error(err)
 
